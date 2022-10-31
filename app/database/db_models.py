e9d8a1c76e291e70cecd665523eca3d99fd5d4b2
BSaaber <maxim-movshin@yandex.ru>
2022-10-31 13:44:42 +0300

    init commit

--- app/database/db_models.py
+++ app/database/db_models.py
@@ -10,3 +10,23 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     level = Column(Integer)
+
+
+class TsnPiece(Base):
+    __tablename__ = "tsn"
+
+    # название сборника?
+    # доп информация джсоном?
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, unique=True, index=True)  # шифр
+    text = Column(String)  # наименование работ и затрат
+    price = Column(Integer)  # всего затрат в текущем уровне, руб
+    # TODO Добавить метч с SpgzPiece. Возможно храним список даже, а не единственное число. т е многие ко многим
+
+
+class SpgzPiece(Base):
+    __tablename__ = "spgz"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    # TODO поля
