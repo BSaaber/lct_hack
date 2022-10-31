27010f3b9ac8a2a897d63e6125bad168240a3b4a
BSaaber <maxim-movshin@yandex.ru>
2022-10-31 13:28:16 +0300

    init tsn parser

---
+++ process_smeta_algorithm/main.py
@@ -0,0 +1,25 @@
+from openpyxl import load_workbook
+
+
+def parse_tsn(filename: str):
+    workbook = load_workbook(filename)
+    worksheet = workbook.active
+    i = 0
+    row_index = 28
+    result = []
+    while row_index < 83:
+        t = {}
+        t["first"] = worksheet.cell(row_index, 1).value
+        t["code"] = worksheet.cell(row_index, 2).value
+        t["text"] = worksheet.cell(row_index, 5).value
+        result.append(t)
+        row_index += 1
+        while worksheet.cell(row_index, 1).value is None:
+            row_index += 1
+        t["summ"] = worksheet.cell(row_index - 2, 27).value
+
+    for i in result:
+        print(i)
+
+
+parse_tsn("tsn1.xlsx")
